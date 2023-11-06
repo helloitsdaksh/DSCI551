97ad6b7b62eb195f16241595b31730065dea5f88
zoohta <koh.0430.ta@gmail.com>
2023-11-05 18:50:46 -0800

    add update functions

--- nosql/tests/functions.py
+++ nosql/tests/functions.py
@@ -1,3 +1,10 @@
+"""
+Implementing CRUD Operations
+For now, I assume that the data is stored in a single Json file
+I will modify the functions to iterate over multiple files later
+"""
+
+
 import json
 import os
 
@@ -68,6 +75,22 @@ def insert_many(database: str, collection: str, new_data: str) -> bool:
     return True
 
 
+def match_nested_condition(item: dict, condition: dict) -> bool:
+    """
+    Check if a condition matches in a JSON object in a nested manner
+    :param item: The JSON object to be checked
+    :param condition: The condition to be checked
+    :return: True if the condition matches, False otherwise
+    """
+    for key, value in condition.items():
+        if isinstance(value, dict):
+            if key not in item or not match_nested_condition(item[key], value):
+                return False
+        elif item.get(key) != value:
+            return False
+    return True
+
+
 def delete_one(database: str, collection: str, condition: str) -> bool:
     """
     Deletes a first itme that matches the given condition from a collection in a JSON database file.
@@ -85,21 +108,16 @@ def delete_one(database: str, collection: str, condition: str) -> bool:
         return False
 
     # Find the first item that matches the condition
-    deleted = False
     for item in data:
-        if all(item.get(key) == value for key, value in json.loads(condition).items()):
+        if match_nested_condition(item, json.loads(condition)):
             data.remove(item)
-            deleted = True
-            break
+            with open(file_name, 'w') as file:
+                json.dump(data, file, indent=2)
+            print('Deletion successful.')
+            return True
 
-    if not deleted:
-        print('Item matching condition not found.')
-        return False
-    else:
-        with open(file_name, 'w') as file:
-            json.dump(data, file, indent=2)
-        print('Deletion successful.')
-        return True
+    print('Item matching condition not found.')
+    return False
 
 
 def delete_many(database: str, collection: str, condition: str) -> bool:
@@ -108,7 +126,7 @@ def delete_many(database: str, collection: str, condition: str) -> bool:
     :param database: The name of the database.
     :param collection: The name of the collection within the database.
     :param condition: The data to be inserted as a JSON object.
-    :return:
+    :return: True if the deletion was successful, False otherwise.
     """
     file_name = f'../data/{database}_{collection}.json'
     try:
@@ -118,15 +136,10 @@ def delete_many(database: str, collection: str, condition: str) -> bool:
         return False
 
     # Find the items that match the condition
-    deleted = False
-    to_keep = data.copy()
-    for item in data:
-        if all(item.get(key) == value for key, value in json.loads(condition).items()):
-            to_keep.remove(item)
-            deleted = True
+    to_keep = [item for item in data if not match_nested_condition(item, json.loads(condition))]
 
-    if not deleted:
-        print('Item matching condition not found.')
+    if len(data) == len(to_keep):
+        print('No items matching the condition found.')
         return False
     else:
         with open(file_name, 'w') as file:
@@ -135,13 +148,90 @@ def delete_many(database: str, collection: str, condition: str) -> bool:
         return True
 
 
+def update_nested_item(item: dict, condition: dict, new_data: dict) -> None:
+    """
+    Updates a nested item in a JSON object
+    :param item:
+    :param condition:
+    :param new_data:
+    :return:
+    """
+    for key, value in condition.items():
+        if isinstance(value, dict) and key in item:
+            update_nested_item(item[key], value, new_data)
+        else:
+            for new_key, new_value in new_data.items():
+                item[new_key] = new_value
+
+
+def update_one(database: str, collection: str, condition: str, new_data: str) -> bool:
+    """
+    Updates a first itme that matches the given condition in a collection in a JSON database file.
+    :param database:
+    :param collection:
+    :param condition:
+    :return: True if the update was successful, False otherwise.
+    """
+    file_name = f'../data/{database}_{collection}.json'
+    try:
+        with open(file_name, 'r') as file:
+            data = json.load(file)
+    except FileNotFoundError:
+        return False
+
+    for item in data:
+        if match_nested_condition(item, json.loads(condition)):
+            update_nested_item(item, json.loads(condition), json.loads(new_data))
+            with open(file_name, 'w') as file:
+                json.dump(data, file, indent=2)
+            return True
+
+    print('Item matching condition not found.')
+    return False
+
+
+def update_many(database: str, collection: str, condition: str, new_data: str) -> bool:
+    """
+    Updates multiple items that match the given condition in a collection in a JSON database file.
+    :param database:
+    :param collection:
+    :param condition:
+    :param new_data:
+    :return: True if the update was successful, False otherwise.
+    """
+    file_name = f'../data/{database}_{collection}.json'
+    try:
+        with open(file_name, 'r') as file:
+            data = json.load(file)
+    except FileNotFoundError:
+        return False
+
+    updated = False
+    for item in data:
+        if match_nested_condition(item, json.loads(condition)):
+            update_nested_item(item, json.loads(condition), json.loads(new_data))
+            updated = True
+
+    if not updated:
+        print('No items matching the condition found.')
+        return False
+    else:
+        with open(file_name, 'w') as file:
+            json.dump(data, file, indent=2)
+        print('Update successful.')
+        return True
+
+
 if __name__ == '__main__':
     database = 'sample'
     collection = 'test'
-    # new_data = '{"key1": "value1", "key2": "value2"}'
+    # new_data = '{"key1": {"key6": "value6"}, "key2": "value2"}'
     # insert_one(database, collection, new_data)
     # l_data = '[{"key3": "value3", "key4": "value4"},{"key3": "value3", "key4": "value4"}]'
     # insert_many(database, collection, l_data)
-    condition = '{"key1": "value1"}'
-    delete_one(database, collection, condition)
+    condition = '{"key3": "value3"}'
+    # delete_one(database, collection, condition)
     # delete_many(database, collection, condition)
+    update_data = '{"key5": "value6"}'
+    # update_one(database, collection, condition, update_data)
+    update_many(database, collection, condition, update_data)
