f254ae9b6eda9afb58b74098462fa7faa3883a5f
zoohta <koh.0430.ta@gmail.com>
2023-11-16 14:49:50 -0800

    update command validation

--- nosql/cli.py
+++ nosql/cli.py
@@ -1,71 +1,107 @@
+from nosql.functions import *
+from nosql.query_executor import *
 import json
-from tests.functions import *
+import re
+
+METADATA_FILE = 'metadata.json'
 
 
 def validate_command(command: str):
     global selected_db
-    command_parts = command.split()
+    # Splitting only on the first two spaces to get the command and collection name
+    parts = command.split(' ', 2)
+    for_query = command.split(' ', 1)
 
-    if command_parts[0] == 'select_db':
-        if len(command_parts) != 2:
+    if not parts or len(parts) < 2:
+        print("Invalid command. Please enter a valid command.")
+        return
+
+    command_type = parts[0]
+
+    if command_type == 'select_db':
+        if len(parts) != 2:
             print("Invalid command. Usage: select_db <database_name>")
         else:
-            selected_db = command_parts[1]
-            print(f"Selected database: {selected_db}")
-            # need to check if the database exists
+            selected_db = parts[1]
+            db_exists = False
+            with open(METADATA_FILE, 'r') as file:
+                metadata = json.load(file)
+                for db in metadata['databases']:
+                    if db['name'] == selected_db:
+                        print(f"Selected database: {selected_db}")
+                        db_exists = True
+                        break
+                if not db_exists:
+                    print(f"Database '{selected_db}' does not exist.")
+
     elif not selected_db:
         print("No database selected. Please select a database using 'select_db <database_name>'")
+
     else:
-        if command_parts[0] == 'insert':
-            if len(command_parts) != 3:
+        if command_type == 'insert':
+            if len(parts) != 3:
                 print("Invalid command. Usage: insert <collection_name> <data>")
             else:
-                collection_name = command_parts[1]
-                data = command_parts[2]
-                insert_one(selected_db, collection_name, data)
+                collection_name = parts[1]
+                data = parts[2]
+                res = insert_one(selected_db, collection_name, data)
+                if res:
+                    print("Insertion successful.")
 
-        elif command_parts[0] == 'insertMany':
-            if len(command_parts) != 3:
+        elif command_type == 'insertMany':
+            if len(parts) != 3:
                 print("Invalid command. Usage: insert_many <collection_name> <data>")
             else:
-                collection_name = command_parts[1]
-                data = command_parts[2]
-                insert_many(selected_db, collection_name, data)
+                collection_name = parts[1]
+                data = parts[2]
+                res = insert_many(selected_db, collection_name, data)
+                if res:
+                    print("Insertion successful.")
 
-        elif command_parts[0] == 'update':
-            if len(command_parts) != 4:
+        elif command_type == 'modify':
+            # Splitting the third part to get the condition and data
+            condition_and_data = re.findall(r'\{.*?\}', parts[2])
+            if len(condition_and_data) != 2:
                 print("Invalid command. Usage: update <collection_name> <condition> <data>")
             else:
-                collection_name = command_parts[1]
-                condition = command_parts[2]
-                data = command_parts[3]
+                collection_name = parts[1]
+                condition = condition_and_data[0]
+                data = condition_and_data[1]
                 update_one(selected_db, collection_name, condition, data)
 
-        elif command_parts[0] == 'updateMany':
-            if len(command_parts) != 4:
-                print("Invalid command. Usage: update_many <collection_name> <condition> <data>")
+        elif command_type == 'modifyMany':
+            # Splitting the third part to get the condition and data
+            condition_and_data = re.findall(r'\{.*?\}', parts[2])
+            if len(condition_and_data) != 2:
+                print("Invalid command. Usage: update <collection_name> <condition> <data>")
             else:
-                collection_name = command_parts[1]
-                condition = command_parts[2]
-                data = command_parts[3]
+                collection_name = parts[1]
+                condition = condition_and_data[0]
+                data = condition_and_data[1]
                 update_many(selected_db, collection_name, condition, data)
 
-        elif command_parts[0] == 'delete':
-            if len(command_parts) != 3:
+        elif command_type == 'remove':
+            if len(parts) != 3:
                 print("Invalid command. Usage: delete <collection_name> <condition>")
             else:
-                collection_name = command_parts[1]
-                condition = command_parts[2]
+                collection_name = parts[1]
+                condition = parts[2]
                 delete_one(selected_db, collection_name, condition)
 
-        elif command_parts[0] == 'deleteMany':
-            if len(command_parts) != 3:
+        elif command_type == 'removeMany':
+            if len(parts) != 3:
                 print("Invalid command. Usage: delete_many <collection_name> <condition>")
             else:
-                collection_name = command_parts[1]
-                condition = command_parts[2]
+                collection_name = parts[1]
+                condition = parts[2]
                 delete_many(selected_db, collection_name, condition)
 
+        elif command_type == 'find':
+            if len(for_query) != 2:
+                print("Invalid command. Usage: query <query>")
+            else:
+                query = for_query[1]
+                execute_query(selected_db, query)
         else:
             print("Invalid command. Please try again.")
 
